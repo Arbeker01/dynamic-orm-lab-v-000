4802986cf408200ed6c6ff0b35c2d011f7113cb4
Romeo Gono <romeogono1@aol.com>
2018-08-21 01:29:13 +0000

    automatically backed up by learn

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -5,4 +5,14 @@ class InteractiveRecord
   def self.table_name
     "#{self.to_s.downcase}s"
   end
+
+  def self.column_names
+    sql = "pragma tb_info('#{table_name}')"
+    tb_info = DB[:conn].execute(sql)
+    column_names = []
+    tb_info.each do |row|
+      column_names << row["name"]
+    end
+    column_names.compact
+  end
 end
